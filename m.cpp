80fa6bada6430dffbe9c1dfe9b59de4553a5d778
Yang còi <91711649+yangcoi@users.noreply.github.com>
2022-03-28 10:53:35 +0700

    Add files via upload

---
+++ m.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Toán - Tin luôn luôn đơn giản 
+    nhưng people make it complicated nên là 
+    mình cứ enjoy cái moment này!";
+}
+
+
